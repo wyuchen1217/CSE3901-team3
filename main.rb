c957e2034855bbce4b942db9272149a60b7c1237
Hannah Ackley <ackley.77@osu.edu>
2023-02-02 13:36:16 -0500

    Add outline for main for others to implement later

--- main.rb
+++ main.rb
@@ -1,3 +1,31 @@
 # Authors: Hannah Ackley, 
 # Date: 7 February 2023
 
+# Keep playing games until user states that they are done
+loop do
+
+    # Create a new game instance by calling the game constructor
+
+
+    # "Play" game until the timer runs out
+    loop do
+
+        # Output the cards in the hand
+
+        # Prompt player for their guess (guess 0 if no sets in hand)
+
+        # Subtract one point if player incorrectly states that no sets exist
+
+        # Add one point if player correctly identifies a set
+
+        # Subtract one point is player guesses an incorrect set
+
+        # Add 3 additional cards to hand if no set exists
+
+    end
+    # Output the player's number of points earned
+
+    # Ask user if they would like to play again
+
+        # If they do not, output thank you message
+end
