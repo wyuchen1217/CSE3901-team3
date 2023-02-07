c82afa54fcdfc1c4b7142903c867082b5e4a8cd5
Gada2210 <122852311+Gada2210@users.noreply.github.com>
2023-02-06 21:18:28 -0500

    Update deck.rb
    Adding comments!

--- deck.rb
+++ deck.rb
@@ -1,12 +1,16 @@
 require './card'
 
 class Deck
+    #Initialize the deck with all possible combinations.
     def initialize
+        #Initialize empty deck.
         @deck = []
+        #Describing cards possible value 
         @colors = ["red", "green", "purple"]
         @numbers = ["one", "two", "three"]
         @shapes = ["squiggle", "diamond", "oval"]
         @shadings = ["solid", "striped", "open"]
+        #Each attribute combination is looped through to generate a new card instance.
         @colors.each do |color|
             @numbers.each do |number|
                 @shapes.each do |shape|
@@ -18,18 +22,22 @@ class Deck
         end
     end
 
+    #Returns the size of the deck.
     def size
         @deck.length
     end
 
+    #Random shuffle.
     def shuffle
         @deck.shuffle!
     end
 
+    #Dealing cards.
     def deal
         @deck.pop
     end
 
+    #Checking if the deck is empty.
     def empty?
         @deck.empty?
     end
