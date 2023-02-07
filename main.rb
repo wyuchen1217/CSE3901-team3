2f73faf1dc2784e01171562dc4df02b66d6029f4
Hannah Ackley <ackley.77@osu.edu>
2023-02-07 14:28:09 -0500

    Update main to include timer functionality

--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@ require './card.rb'
 require './deck.rb'
 require './player.rb'
 require './game.rb'
+require 'time'
 
 # game set up
 
@@ -20,8 +21,6 @@ player_count.times do
 end
 # puts player_array[0].name
 
-# game = Game.new
-
 # create a new deck
 deck = Deck.new
 deck.shuffle
@@ -29,84 +28,94 @@ deck.shuffle
 # number of cards in one turn
 NUM_CARDS = 12
 
-# Keep playing games until user states that they are done or until deck is empty
+# Keep playing games until user states that they are done
 play_again = true
-until !play_again || deck.empty?
-  
-  # Create a new game instance by calling the game constructor
+until !play_again
 
-  # loop through players
-  player_array.each do |player|
-    puts "#{player.name}'s turn: "
-    puts '________________________'
-    # deal 12 cards from the deck
-    table = []
-    NUM_CARDS.times { table << deck.deal }
+  # Terminate game when timer runs out
+  start_time = Time.now
+  times_up = false
+  until times_up
 
-    # Output the cards in the hand
-    puts 'The cards on the table are: '
-    table.each_with_index do |card, index|
-      puts "#{index + 1}. #{card.output}"
-    end
+    # loop through players
+    player_array.each do |player|
+      puts "#{player.name}'s turn: "
+      puts '________________________'
+      # deal 12 cards from the deck
+      table = []
+      NUM_CARDS.times { table << deck.deal }
 
-    # Prompt player for their guess (guess 0 if no sets in hand)
-    puts 'Pick a card by entering the number seen to the left of the card: '
-    puts 'Pick card 1 (or enter 0 if there is no set. Enter 13 for hint): '
-    input1 = gets.chomp
-    
+      # Output the cards in the hand
+      puts 'The cards on the table are: '
+      table.each_with_index do |card, index|
+        puts "#{index + 1}. #{card.output}"
+      end
 
-    # If 13, hint generated!
-    if input1 == '13'
-      hint(table)
-      puts 'Pick card 1 again!'
+      # Prompt player for their guess (guess 0 if no sets in hand)
+      puts 'Pick a card by entering the number seen to the left of the card: '
+      puts 'Pick card 1 (or enter 0 if there is no set. Enter 13 for hint): '
       input1 = gets.chomp
-    end
+      
 
-    # If 0, skip
-    if input1 == '0'
-      if table_contains_set?(table)
-        # Subtract one point if player incorrectly states that no sets exist
-        puts 'Incorrect, there is a set in the hand. Minus 1 points. Try again.'
-        player.dec_points
-        next
-      else
-        # Add 3 additional cards to hand if no set exists
-        puts 'Correct, there are currently no sets in this hand. 3 more cards will be added.'
-        3.times { table << deck.deal }
-        # Output the cards in the hand
-        puts 'The cards on the table are: '
-        table.each_with_index do |card, index|
-          puts "#{index + 1}. #{card.output}"
+      # If 13, hint generated!
+      if input1 == '13'
+        hint(table)
+        puts 'Pick card 1 again!'
+        input1 = gets.chomp
+      end
+
+      # If 0, skip
+      if input1 == '0'
+        if table_contains_set?(table)
+          # Subtract one point if player incorrectly states that no sets exist
+          puts 'Incorrect, there is a set in the hand. Minus 1 points. Try again.'
+          player.dec_points
+          next
+        else
+          # Add 3 additional cards to hand if no set exists
+          puts 'Correct, there are currently no sets in this hand. 3 more cards will be added.'
+          3.times { table << deck.deal }
+          # Output the cards in the hand
+          puts 'The cards on the table are: '
+          table.each_with_index do |card, index|
+            puts "#{index + 1}. #{card.output}"
+          end
+          
         end
-        
-      end
-    else
-      puts 'Pick card 2: '
-      input2 = gets.chomp
-
-      puts 'Pick card 3: '
-      input3 = gets.chomp
-
-      card1 = table[input1.to_i - 1]
-      card2 = table[input2.to_i - 1]
-      card3 = table[input3.to_i - 1]
-
-      # Add one point if player correctly identifies a set
-      if check_set(card1, card2, card3)
-        player.inc_points
-        puts "Correct! #{card1.output}, #{card2.output}, and #{card3.output} makes a set."
-
-        # Output the player's number of points earned
-        puts "Congrats! You scored #{player.points} points."
-
-      # Subtract one point is player guesses an incorrect set
       else
-        player.dec_points
-        puts "#{card1.output}, #{card2.output}, and #{card3.output} are not a set, try again."
+        puts 'Pick card 2: '
+        input2 = gets.chomp
+
+        puts 'Pick card 3: '
+        input3 = gets.chomp
+
+        card1 = table[input1.to_i - 1]
+        card2 = table[input2.to_i - 1]
+        card3 = table[input3.to_i - 1]
+
+        # Add one point if player correctly identifies a set
+        if check_set(card1, card2, card3)
+          player.inc_points
+          puts "Correct! #{card1.output}, #{card2.output}, and #{card3.output} makes a set."
+
+          # Output the player's number of points earned
+          puts "Congrats! You scored #{player.points} points."
+
+        # Subtract one point is player guesses an incorrect set
+        else
+          player.dec_points
+          puts "#{card1.output}, #{card2.output}, and #{card3.output} are not a set, try again."
+        end
       end
+    end # END OF PLAYER LOOP
+    # Check the time
+    elapsed_time = Time.now - start_time
+    if elapsed_time >= 60
+      times_up = true
+      puts "Time's up!"
     end
-    # END OF PLAYER LOOP
-  end
+  end # END OF GAME LOOP
+
 
   # Ask user if they would like to play again
 
